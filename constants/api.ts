2f30d9d1aa498ab6b6e2786aacf103dcd5910815
Sangam5756 <sangammunde3@gmail.com>
2025-08-14 09:25:08 +0530

    backend url changed

--- constants/api.ts
+++ constants/api.ts
@@ -1,3 +1,3 @@
-// export const API_URL = "https://devflowbackend.sangammundhe.site/api/v1"
-export const API_URL = "https://devflowbackend.onrender.com/api/v1"
+export const API_URL = "https://devflowbackend.sangammundhe.site/api/v1"
+// export const API_URL = "https://devflowbackend.onrender.com/api/v1"
 // export const API_URL = "http://localhost:4000/api/v1"
